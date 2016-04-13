cddb573a9e1c2955a88b7686aea5819a161e4473
iordanis kosoglou <iordanis@eq76uxim.vpn.rrze.uni-erlangen.de>
2016-04-13 17:47:34 +0200

    edit photo tags and visibility

--- app/src/main/java/com/wahlzeit/mobile/activities/EditPhotoActivity.java
+++ app/src/main/java/com/wahlzeit/mobile/activities/EditPhotoActivity.java
@@ -3,19 +3,27 @@ package com.wahlzeit.mobile.activities;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import com.appspot.iordanis_mobilezeit.wahlzeitApi.model.Photo;
+import com.appspot.iordanis_mobilezeit.wahlzeitApi.model.Tags;
 import com.wahlzeit.mobile.R;
 import com.wahlzeit.mobile.WahlzeitModel;
+import com.wahlzeit.mobile.asyncTasks.UpdatePhotoTask;
 import com.wahlzeit.mobile.components.textswitcher.EditorActionListener;
 import com.wahlzeit.mobile.components.textswitcher.FocusChangeListener;
 import com.wahlzeit.mobile.components.textswitcher.TextViewClickListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -32,6 +40,7 @@ public class EditPhotoActivity extends BaseActivity {
     @InjectView(R.id.button_update_photo) Button buttonUpdatePhoto;
     Photo photoToUpdate;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,8 +48,21 @@ public class EditPhotoActivity extends BaseActivity {
         ButterKnife.inject(this);
         getPhoto();
         setupImageView();
+        setupSwitch();
         setupTagsText();
+        buttonUpdatePhoto.setOnClickListener(new UpdatePhotoButtonListener());
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setupSwitch() {
+        switchVisibility.setChecked(photoToUpdate.getVisible());
+        switchVisibility.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                photoToUpdate.setVisible(!photoToUpdate.getVisible());
+                photoToUpdate.setStatus(photoToUpdate.getVisible() ? "VISIBLE" : "INVISIBLE");
+            }
+        });
     }
 
     private void setupImageView() {
@@ -57,7 +79,8 @@ public class EditPhotoActivity extends BaseActivity {
     }
 
     private void setupTagsText() {
-//        String tags = photoToUpdate.getTags().getTags().toArray().toString();
+        String tags = WahlzeitModel.model.getPhotoTagsAsString(photoToUpdate);
+        textViewTags.setText(tags);
         View.OnClickListener onClickListener = new TextViewClickListener(this);
         View.OnFocusChangeListener onFocusChangeListener = new FocusChangeListener(this);
         TextView.OnEditorActionListener onEditorActionListener = new EditorActionListener(this);
@@ -66,10 +89,27 @@ public class EditPhotoActivity extends BaseActivity {
         editTextTags.setOnEditorActionListener(onEditorActionListener);
     }
 
+    private Tags getTagsFromTextView() {
+        Tags result = new Tags();
+        String tagsText = textViewTags.getText().toString();
+        String tagsPlaceholder = getResources().getString(R.string.tags_placeholder);
+        if(tagsText.equals(tagsPlaceholder)) {
+            //do nothing
+            Log.i(this.getTitle().toString(), "do nothing");
+        } else {
+            String[] tagsArray = tagsText.split(",");
+            List<String> tagsList = new ArrayList<String>(Arrays.asList(tagsArray));
+            result.setTags(tagsList);
+        }
+        return result;
+    }
+
     private class UpdatePhotoButtonListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
             // update photo task call
+            photoToUpdate.setTags(getTagsFromTextView());
+            new UpdatePhotoTask(getApplicationContext()).execute(photoToUpdate);
         }
     }
 
---
+++ app/src/main/java/com/wahlzeit/mobile/asyncTasks/UpdatePhotoTask.java
@@ -0,0 +1,47 @@
+package com.wahlzeit.mobile.asyncTasks;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.appspot.iordanis_mobilezeit.wahlzeitApi.WahlzeitApi;
+import com.appspot.iordanis_mobilezeit.wahlzeitApi.model.Photo;
+import com.wahlzeit.mobile.CommunicationManager;
+import com.wahlzeit.mobile.WahlzeitModel;
+
+import java.io.IOException;
+
+/**
+ * Created by iordanis on 13/04/16.
+ */
+public class UpdatePhotoTask extends AsyncTask<Photo, Void, String> {
+
+    Context context;
+
+    public UpdatePhotoTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(Photo... params) {
+        Photo photoToUpdate = params[0];
+        WahlzeitApi wahlzeitServiceHandle = CommunicationManager.manager.getApiServiceHandler(WahlzeitModel.model.getCredential());
+        try {
+            WahlzeitApi.Photos.Update updateCommand = wahlzeitServiceHandle.photos().update(photoToUpdate);
+            Photo updatedPhoto = updateCommand.execute();
+            WahlzeitModel.model.updatePhoto(photoToUpdate);
+        } catch (IOException e) {
+            Log.e("UpdatePhotoTask", e.getMessage());
+        }
+        return "Photo updated successfully";
+    }
+
+    protected void onPostExecute(String result) {
+        CharSequence text = result;
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        // TODO: Update card stack and home list
+    }
+}
